18fb44425976cc3ebbf29f57d3055b76aaf54afe
Tomas Monier <tomasmnr@localhost.localdomain>
2020-02-08 11:51:10 -0300

    custom icon

--- lib/main.dart
+++ lib/main.dart
@@ -16,7 +16,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'SeSale',
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.green,
       ),
       home: Home(),
     );
